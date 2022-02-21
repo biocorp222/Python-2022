bf36d1f33b189a9992f4243dc14da29e59aa6342
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-02-22 12:14:11 +1300

    first exercise

---
+++ 1_print_exercises.py
@@ -0,0 +1,22 @@
+#1 print out your name
+print("Dan Smith")
+#2 print your teachers name
+print("Luke Smith")
+#3 print out your school name
+print("Kings High School")
+#4 print out your age
+print("15")
+#5 print how any hours you have in your favourite game
+print("300")
+#6 print what year it will be in 25 years
+print(2022 + 25)
+#7 print how old you will be in 100 years
+print(15+100)
+#8 print out 658 divided by 7 
+print(658/7)
+#9 how many years ago was the moon landing
+print(2022-1969)
+#Print out your favourite game
+print("NFS Heat")
+#pritn the name of your favourite class
+print("DTE")
