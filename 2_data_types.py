f0b53fecd3db8a55f4377d4899dbfe69c476e92b
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-02-24 14:51:51 +1300

    exercise 2

---
+++ 2_data_types.py
@@ -0,0 +1,16 @@
+#1
+print("I said, ooh, I'm blinded by the lights\nNo, I can't sleep until I feel your touch\nI said, ooh, I'm drowning in the night\nOh, when I'm like this, you're the one I trust")
+#2
+print('\'to infinity and beyond\'')
+#3
+print("Strings are a collection of characters – these characters can be letters, symbols or even numbers.")
+#4
+print("Integers are whole numbers (4, 100, -4)")
+#5
+print("floats contain a “floating point decimal” (4.3, 2.1, 10.0).")
+#6
+print(543/3)
+#7
+print(543/3)
+#8
+print(543/3)
