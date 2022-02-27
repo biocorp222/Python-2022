b53d7df8fa69d294dc1524b90432bfac74ebfa49
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-02-28 09:52:29 +1300

    exercise 3

---
+++ 3_Variable_Practice.py
@@ -0,0 +1,22 @@
+#1
+player_lives = 0
+#2
+login = rgsryk
+#3
+money = 1351
+#4
+Phone_Number = 021 120 8757
+#5
+Name = "Daniel"
+#6
+age = 15
+#7
+Current_year = 2022
+#8
+print (name)
+#9
+print(age)
+#10
+print(Current_year)
+#11
+last_name = "smith"
