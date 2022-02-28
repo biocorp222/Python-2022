9b5b14c3678d3cd8f7c2dadb7548da6f77e0f584
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-01 10:16:19 +1300

    exercise 3

--- 3_Variable_Practice.py
+++ 3_Variable_Practice.py
@@ -1,22 +1,45 @@
 #1
 player_lives = 0
 #2
-login = rgsryk
+login = 0000
 #3
 money = 1351
 #4
-Phone_Number = 021 120 8757
+phone_Number = "121 120 8757"
 #5
-Name = "Daniel"
+name="Daniel"
 #6
 age = 15
 #7
-Current_year = 2022
+current_year = 2022
 #8
 print (name)
 #9
 print(age)
 #10
-print(Current_year)
+print(current_year)
 #11
 last_name = "smith"
+#12
+school_name = "Kings high school"
+#13
+fav_food = "pizza"
+#14
+animal = "lion"
+#15
+eaten_choclate = 0
+#16
+player_score = 000
+#17
+player_highscore = 0
+#18
+completed_laps = 0
+#19
+ammo = 24
+#20
+current_gear = 3
+#21
+current_speed = 120
+
+
+
