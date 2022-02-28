91ed15f58c2ddd1070825941b47046479c25445b
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-01 10:24:22 +1300

    exercise 4

---
+++ 4_modifying_variable.py
@@ -0,0 +1,38 @@
+money = 200
+hat = 20 
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+
+print("buying the hat")
+
+money -= hat
+print("you have the following ammount left over")
+print(money)
+
+print("buying top")
+money -= top
+print("you have the following ammount left over")
+print(money)
+
+print("buying the pants")
+money -= pants
+print("you have the following ammount left over")
+print(money)
+
+print("buying the pants")
+money -= pants
+print("you have the following ammount left over")
+print(money)
+
+print("buying the belt")
+money -= belt
+print("you have the following ammount left over")
+print(money)
+
+print("buying the shoes")
+money -= shoes
+print("you have the following ammount left over")
+print(money)
+
