8eb93b878c34792bb478d49d4d3eb7ce75d775aa
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-01 10:56:51 +1300

    exercise 5

---
+++ 5_input.py
@@ -0,0 +1,43 @@
+#1
+#key = input("press any key and then press enter")
+#2
+#input("enter your name then press enter")
+#3
+#input("enter your age then press enter")
+#4
+#name = input("enter yur name then press enter")
+#5
+#age = input("enter your age then press enter")
+#6
+#fav_movie = input("whats your favourite movie")
+#7
+#book = input("name any book")
+#8
+#adjective = input("type any adjective")
+#9
+#noun = input("type any noun")
+#10
+#verb = input("type any verb")
+#11
+#print (f"Hi {name}, you are {age}, years old")
+#print(f"you like {fav_movie} and have read {book}")
+#print(f"some words you thought of are {verb} {adjective} {noun}")
+#12
+age = int(input("whats your age"))
+#13
+ten_years = age + 10
+print(f"you will be {ten_years}  in ten years")
+#14
+birth_year = 2022 - age
+print(f"you were born in {birth_year} ")
+#15
+apples = int(input("how may apples do you have"))
+#16
+friends = int(input("how many friends so you have?"))
+#17
+shared = apples//friends
+print(f"your friends can have {shared} each")
+#18
+pizza = int(input("how many pizza do you want"))
+#19
+people =  
