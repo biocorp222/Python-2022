242f7f2675173b49596f87ab92e933d34971689e
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-09 10:49:11 +1300

    exercise7

---
+++ 7_lists.py
@@ -0,0 +1,6 @@
+fibonacci = [0,1,1,2,3,5,8,13,21,34]
+print(fibonacci)
+fruit = ["apple","banana","orange","grapefruit","watermelon"]
+print(fruit)
+youtubers = ["Lukers","pewdiepie","Jacksepticeye","Sidemen"]
+print(youtubers)
