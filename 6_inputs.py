6bf034b3687fbe177d7ca2bf557b5d8d3f07e037
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-09 10:18:00 +1300

    exercise 6

---
+++ 6_inputs.py
@@ -0,0 +1,35 @@
+#icecreams = int(input("how many ice creams do you want"))
+#if  icecreams > 20:
+#	print("we do not have enough ice cream")
+#else:
+#	print("here are your ice creams")
+
+#travel = int(input("how far do you intend to travel"))
+#if travel > 200:
+#	print("remember to fuel up")
+#else:
+#	print("have fun")
+
+#age = int(input("how old are you?"))
+#if age >= 18:
+#	print("your an adult")
+#else:
+#	print("your a minor")
+
+#movie = (input("what is your favourite movie"))
+#if movie == ("lord of the rings"):
+#	print("you have excellent taste")
+#else:
+#	print("Lord of the rings is clearly better")
+
+#darth = input("have you heard of darth plagueis")
+#if darth == ("no") or ("No"):
+	#print("The Tragedy of Darth Plagueis the Wise was a Sith legend that was relayed to Anakin Skywalker by Palpatine, telling of his master, Darth Plagueis. Plagueis, the legend went, was so powerful and so wise he could use the Force to influence the midi-chlorians to create life, and even saved others from dying. He became so powerful, the only thing that he was afraid of was losing his power. His apprentice Darth Sidious recalled. In the end, Sidious, though unnamed by Palpatine in his recounting of the story to Anakin—killed him in his sleep. According to Palpatine, what made it a tragedy was the irony that he could not save himself from his death, despite being able to save others. When Palpatine told this story to Skywalker, Skywalker countered that it was only a tragedy for Plagueis, and for the apprentice it was a happy ending. Palpatine later revealed that he was the unnamed apprentice, and that Plagueis' ability had been real. This was one of the steps towards turning Skywalker to the dark side of the Force.")
+#else:
+#	print("You must be a fan?")
+
+mel = input("Who directed pasion of the christ")
+if mel.lower == ("mel gibson"):
+	print("correct")
+else:
+	print("it was mel gibson")
--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-#Test python file
+#T
