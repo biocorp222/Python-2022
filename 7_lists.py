7052f5558b52610605b2e2d4aeef3fdd39f898de
biocorp222 <63990849+biocorp222@users.noreply.github.com>
2022-03-10 11:53:33 +1300

    7

--- 7_lists.py
+++ 7_lists.py
@@ -4,3 +4,31 @@ fruit = ["apple","banana","orange","grapefruit","watermelon"]
 print(fruit)
 youtubers = ["Lukers","pewdiepie","Jacksepticeye","Sidemen"]
 print(youtubers)
+
+songs = []
+songs.append("Ciggarettes, juice WRLD")
+songs.append("Liporice, 80purppp")
+songs.append("F**k the world, brent Faiyas")
+songs.append("no role modelz, j cole")
+songs.append("real friends, kayne west")
+
+books = []
+print("what are your 5 favourite books")
+books.append(input("enter the name of book 1"))
+books.append(input("enter the name of book 2"))
+books.append(input("enter the name of book 3"))
+books.append(input("enter the name of book 4"))
+books.append(input("enter the name of book 5"))
+print(books)
+
+pizza = []
+pritn("what pizza toppings do you want")
+pizza.append(input("enter topping #1"))
+pizza.append(input("enter topping #2"))
+pizza.append(input("enter topping #3"))
+pizza.append(input("enter topping #4"))
+pizza.append(input("enter topping #5"))
+pizza.append(input("enter topping #6"))
+pizza.remove(" ")
+pizza.remove("")
+print(pizza)
